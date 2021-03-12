3c3b969b6abbcf106886a4a15519cdbfa1823f2b
rishitachaudhary50 <80538854+rishitachaudhary50@users.noreply.github.com>
2021-03-12 22:56:56 +0530

    Program added.

---
+++ voweloccurence.py
@@ -0,0 +1,18 @@
+def vowelCount(ip_str):
+
+    
+    vowels = 'aeiou'
+
+
+	ip_str = ip_str.casefold()
+
+    count = {}.fromkeys(vowels,0)
+
+    for char in ip_str:
+        if char in count:
+            count[char] += 1
+
+    print(count)
+    
+    
+vowelCount('Hello, World')
